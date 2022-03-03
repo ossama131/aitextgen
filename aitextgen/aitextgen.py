e887b934edcc7342b3d5831a62b58978353b9396
Tung Son Tran <44701960+JohanObluda@users.noreply.github.com>
2022-03-03 15:39:30 +0100

    Revert "Change backend strategy to ddp2"
    This reverts commit 4213eca0db24e6b65262e81ca770f72c842e9e19.

--- aitextgen/aitextgen.py
+++ aitextgen/aitextgen.py
@@ -748,7 +748,7 @@ class aitextgen:
             train_params["benchmark"] = True
 
         if n_gpu==-1 or n_gpu > 1:
-            train_params["strategy"] = "ddp2"
+            train_params["strategy"] = "ddp"
 
         trainer = pl.Trainer(**train_params)
         trainer.fit(train_model)
