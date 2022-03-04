a2d40205f7669fa71525d5b65c8f7fe3f06ae46d
Tung Son Tran <44701960+JohanObluda@users.noreply.github.com>
2022-03-05 00:55:50 +0100

    Update aitextgen.py
    Minor fix

--- aitextgen/aitextgen.py
+++ aitextgen/aitextgen.py
@@ -553,10 +553,10 @@ class aitextgen:
     def train(
         self,
         train_data: Union[str, TokenDataset],
+        n_gpu: int = Union[int,str]=-1,
         output_dir: str = "trained_model",
         fp16: bool = False,
-        fp16_opt_level: str = "O1",
-        n_gpu: int = Union[int,str]=-1,
+        fp16_opt_level: str = "O1",       
         tpu_cores: int = 0,
         max_grad_norm: float = 0.5,
         gradient_accumulation_steps: int = 1,
